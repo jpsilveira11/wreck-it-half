c2ed38706ae3ab97e9bc9909d4145de6bca41a58
João Pedro E. Silveira <108689656+jpsilveira11@users.noreply.github.com>
2023-11-27 15:11:57 -0300

    moving target

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,40 @@
-alert('HEY!')
+const state={
+
+    view:{
+        squares:document.querySelectorAll(".square"),
+        target:document.querySelector(".target"),
+        time:document.querySelector("#time"),
+        score:document.querySelector("#score"),
+        lifes:document.querySelector("#lifes"),
+    },
+    values:{
+        timer:null,
+        gameSpeed:1000,
+    },
+
+};
+
+function setTarget(){
+    state.view.squares.forEach((square)=>{
+        square.classList.remove("target");
+    })
+    let randomNumber=Math.floor(Math.random()*9);
+    let randomSquare=state.view.squares[randomNumber];
+    randomSquare.classList.add("target");
+}
+function moveTarget(){
+    state.values.timer=setInterval(setTarget,state.values.gameSpeed);
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square)=>{
+        //if(square.id===)
+    })
+}
+
+function runEngine(){
+    // alert("running.");
+    moveTarget();
+}
+
+runEngine();
